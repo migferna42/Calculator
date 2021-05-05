bb0bfb5db25b4971d0868bb990ebc34779ce559a
Miguel angel Fernandez carrillo <migferna@student.42madrid.com>
2021-05-05 11:23:02 +0200

    Clear function & Add function to display

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react';
 import "./Button.css"
 
 const ft_isdigit = val => {
     return !isNaN(val) || val === "." || val === "=";
 }
 
-const Button = props => (
-    <div className={`button-wrapper ${ft_isdigit(props.children) ? null : "operator"}`}>
+export const Button = props => (
+    <div className={`button-wrapper ${ft_isdigit(props.children) ? null : "operator"}`} onClick={() => props.handleClick(props.children)}>
         {props.children}
     </div>);
-
-export default Button;
